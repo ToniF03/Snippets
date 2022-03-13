b5930998d5fe045b93a3d143f05c009b587e0a67
ToniF03 <44695452+ToniF03@users.noreply.github.com>
2022-03-13 17:01:01 +0100

    Update RegistryWatcher.cs

--- RegistryWatcher.cs
+++ RegistryWatcher.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Windows.Threading;
 
@@ -16,23 +16,54 @@ namespace uCalc.Tools
             get { return _val; }
             set { _val = value; ValueChanged.Invoke(this, EventArgs.Empty); }
         }
+        public bool IsEnabled
+        {
+            get { return Timer.IsEnabled; }
+            set { Timer.IsEnabled = value; }
+        }
+        public TimeSpan Interval
+        {
+            get { return Timer.Interval; }
+            set { Timer.Interval = value; }
+        }
 
+        /// <summary>
+        /// A watcher for a special value of a registry key.
+        /// </summary>
+        /// <param name="key">The path to the key.</param>
+        /// <param name="valueName">The name of the observed value.</param>
         public RegistryWatcher(string key, string valueName)
         {
             Timer.Tick += Timer_Tick;
             Timer.Interval = TimeSpan.FromMilliseconds(200);
-            if (key != "" && valueName != "")
+            if (key != "" && valueName != "" && key != null && valueName != null)
             {
                 Key = key;
                 ValueKey = valueName;
             }
             else
-                throw new ArgumentException();
+                throw new ArgumentNullException();
             RegistryKey regKey = Registry.LocalMachine.OpenSubKey(Key);
             if (regKey != null)
                 throw new ArgumentException();
             _val = Registry.GetValue(Key, ValueKey, (object)null).ToString();
             Timer.Start();
+        }
+
+        /// <summary>
+        /// Starts the registry watcher.
+        /// </summary>
+        public void Start()
+        {
+            Timer.Start();
+        }
+
+        /// <summary>
+        /// Stops the registry watcher.
+        /// </summary>
+        public void Stop()
+        {
+            Timer.Stop();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
