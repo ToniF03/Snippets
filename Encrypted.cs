69f8d48f3d775de5ea8f76b38c1e5b645ede46e4
Toni Fey <44695452+ToniF03@users.noreply.github.com>
2025-09-20 10:38:08 +0200

    Create Encrypted.cs

---
+++ Encrypted.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class SaveEncryption
+{
+    private static readonly string key = "Your32CharKeyHere1234567890abcd"; // Must be 32 chars
+    private static readonly string iv = "Your16CharIVHere"; // Must be 16 chars
+
+    public static byte[] Encrypt(string plainText)
+    {
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+
+        using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream();
+        using var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+        using (var sw = new StreamWriter(cs))
+        {
+            sw.Write(plainText);
+        }
+        return ms.ToArray();
+    }
+
+    public static string Decrypt(byte[] cipherData)
+    {
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+
+        using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream(cipherData);
+        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+        using var sr = new StreamReader(cs);
+        return sr.ReadToEnd();
+    }
+}
