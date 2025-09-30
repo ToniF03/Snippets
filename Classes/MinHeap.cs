0df5e96182d1a34b0b753eaf7ade811f0475113e
Toni Fey <44695452+ToniF03@users.noreply.github.com>
2025-09-30 09:58:28 +0200

    Update MinHeap.cs

--- Classes/MinHeap.cs
+++ Classes/MinHeap.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Assets.Scripts.Classes.MinHeap
+namespace uClasses.MinHeap
 {
     /// <summary>
     /// A generic MinHeap implementation for managing elements with priority.
@@ -9,37 +9,60 @@ namespace Assets.Scripts.Classes.MinHeap
     /// <typeparam name="T">The type of elements in the heap, must implement IComparable&lt;T&gt;.</typeparam>
     public class MinHeap<T> where T : IComparable<T>
     {
+        // Internal list to store heap elements.
         private List<T> _items = new List<T>();
+
+        // Dictionary to track the position of each element in the heap for fast lookup and updates.
         private Dictionary<T, int> _positions = new Dictionary<T, int>();
 
+        // Gets the number of elements in the heap.
         public int Count => _items.Count;
 
+        /// <summary>
+        /// Adds a new item to the heap and maintains the heap property.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
         public void Add(T item)
         {
             _items.Add(item);
             int index = _items.Count - 1;
             _positions[item] = index;
-            BubbleUp(index);
+            BubbleUp(index); // Restore heap property upwards.
         }
 
+        /// <summary>
+        /// Removes and returns the smallest item (root) from the heap.
+        /// </summary>
+        /// <returns>The smallest item in the heap.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the heap is empty.</exception>
         public T Pop()
         {
             if (_items.Count == 0) throw new InvalidOperationException("Heap is empty");
 
             T root = _items[0];
-            T last = _items[^1];
+            T last = _items[_items.Count - 1];
             _items[0] = last;
             _positions[last] = 0;
 
             _items.RemoveAt(_items.Count - 1);
             _positions.Remove(root);
 
-            if (_items.Count > 0) BubbleDown(0);
+            if (_items.Count > 0) BubbleDown(0); // Restore heap property downwards.
             return root;
         }
 
+        /// <summary>
+        /// Checks if the heap contains the specified item.
+        /// </summary>
+        /// <param name="item">The item to check for.</param>
+        /// <returns>True if the item exists in the heap; otherwise, false.</returns>
         public bool Contains(T item) => _positions.ContainsKey(item);
 
+        /// <summary>
+        /// Updates the position of an item in the heap after its priority has changed.
+        /// </summary>
+        /// <param name="item">The item whose priority has changed.</param>
+        /// <exception cref="InvalidOperationException">Thrown if the item is not found in the heap.</exception>
         public void UpdateKey(T item)
         {
             if (!_positions.TryGetValue(item, out int index))
@@ -51,6 +74,10 @@ namespace Assets.Scripts.Classes.MinHeap
             BubbleDown(index);
         }
 
+        /// <summary>
+        /// Moves the item at the specified index up the heap until the heap property is restored.
+        /// </summary>
+        /// <param name="index">The index of the item to move up.</param>
         private void BubbleUp(int index)
         {
             while (index > 0)
@@ -63,6 +90,10 @@ namespace Assets.Scripts.Classes.MinHeap
             }
         }
 
+        /// <summary>
+        /// Moves the item at the specified index down the heap until the heap property is restored.
+        /// </summary>
+        /// <param name="index">The index of the item to move down.</param>
         private void BubbleDown(int index)
         {
             int lastIndex = _items.Count - 1;
@@ -84,6 +115,11 @@ namespace Assets.Scripts.Classes.MinHeap
             }
         }
 
+        /// <summary>
+        /// Swaps two items in the heap and updates their positions in the dictionary.
+        /// </summary>
+        /// <param name="a">The index of the first item.</param>
+        /// <param name="b">The index of the second item.</param>
         private void Swap(int a, int b)
         {
             T tmp = _items[a];
